dc135c02522d773a4424a9e7427461fabd0a5398
Viraat Chandra <viraat_chandra_24@berkeley.edu>
2019-12-07 18:44:37 -0800

    added modules and env configuration

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/ViRu-ThE-ViRuS/Gunther
+
+go 1.13
+
+require github.com/joho/godotenv v1.3.0
--- init.go
+++ init.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+var (
+	args = os.Args[1:]
+)
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Println("no .enf file found")
+	}
+
+	if len(os.Getenv("DEBUG")) == 0 {
+		log.SetOutput(ioutil.Discard)
+	}
+}
 
 func main() {
-	fmt.Println("hello world")
+	log.Printf("arguments: %v", args)
 }
